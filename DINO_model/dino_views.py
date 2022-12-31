278490e6394e4dee450ee51f25c0892b74505001
hanjoo0211 <hanjoo0211@gmail.com>
2023-01-01 01:51:20 +0900

    chore: add try-except and message for no plant

--- DINO_model/dino_views.py
+++ DINO_model/dino_views.py
@@ -40,85 +40,86 @@ with open('../DINO_model/util/20class_plant_coco_id2name.json') as f:
 
 def dino_api(request):
     utterance = json.loads(request.body)['userRequest']['utterance']
-    # try:
-    if request.method == 'POST':
-        imageURL = utterance
-        openedURL = urllib.request.urlopen(imageURL)
-        with open('image.jpeg', 'wb') as f:
-            f.write(openedURL.read())
+    try:
+        if request.method == 'POST':
+            imageURL = utterance
+            openedURL = urllib.request.urlopen(imageURL)
+            with open('image.jpeg', 'wb') as f:
+                f.write(openedURL.read())
 
-        image = Image.open('image.jpeg').convert("RGB")
-        imgDPI = 96
-        imgWidth, imgHeight = image.size
+            image = Image.open('image.jpeg').convert("RGB")
+            imgDPI = 96
+            imgWidth, imgHeight = image.size
 
-        transform = T.Compose([
-            T.RandomResize([800], max_size=1333),
-            T.ToTensor(),
-            T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-        ])
-        image, _ = transform(image, None)
+            transform = T.Compose([
+                T.RandomResize([800], max_size=1333),
+                T.ToTensor(),
+                T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            ])
+            image, _ = transform(image, None)
 
-        model.eval()
-        with torch.no_grad():
-            output = model.cuda()(image[None].cuda())
-            output = postprocessors['bbox'](output, torch.Tensor([[1.0, 1.0]]).cuda())[0]
+            model.eval()
+            with torch.no_grad():
+                output = model.cuda()(image[None].cuda())
+                output = postprocessors['bbox'](output, torch.Tensor([[1.0, 1.0]]).cuda())[0]
 
-        threshold = 0.2 # threshold 조절
+            threshold = 0.2 # threshold 조절
 
-        vslzr = COCOVisualizer()
+            vslzr = COCOVisualizer()
 
-        scores = output['scores']
-        labels = output['labels']
-        boxes = box_ops.box_xyxy_to_cxcywh(output['boxes'])
-        select_mask = scores > threshold
-        print(scores[select_mask])
+            scores = output['scores']
+            labels = output['labels']
+            boxes = box_ops.box_xyxy_to_cxcywh(output['boxes'])
+            select_mask = scores > threshold
+            print(scores[select_mask])
 
-        # 같은 bbox에 중복 label 제거
-        dupBoxes = boxes[select_mask].tolist()
-        for i, bbox in enumerate(dupBoxes):
-            if bbox in dupBoxes[:i]:
-                select_mask[i] = False
+            # 같은 bbox에 중복 label 제거
+            dupBoxes = boxes[select_mask].tolist()
+            for i, bbox in enumerate(dupBoxes):
+                if bbox in dupBoxes[:i]:
+                    select_mask[i] = False
 
-        box_label = [id2name[int(item)] for item in labels[select_mask]]
-        pred_dict = {
-            'boxes': boxes[select_mask],
-            'size': torch.Tensor([image.shape[1], image.shape[2]]),
-            'box_label': box_label
-        }
-        vslzr.visualize(image, pred_dict, savedir='./', dpi=imgDPI, show_in_console=False, width=imgWidth, height=imgHeight)
-        print(box_label)
-        print(pred_dict['boxes'])
+            box_label = [id2name[int(item)] for item in labels[select_mask]]
+            pred_dict = {
+                'boxes': boxes[select_mask],
+                'size': torch.Tensor([image.shape[1], image.shape[2]]),
+                'box_label': box_label
+            }
+            vslzr.visualize(image, pred_dict, savedir='./', dpi=imgDPI, show_in_console=False, width=imgWidth, height=imgHeight)
+            print(box_label)
+            print(pred_dict['boxes'])
 
-        # etc 및 중복 버튼 제거
-        labelJson = []
-        dup = []
-        for i in range(len(box_label)):
-            if box_label[i] not in dup and box_label[i] != 'etc':
-                labelJson.append((box_label[i], str(scores[i])[9:11]))
-                dup.append(box_label[i])
+            # etc 및 중복 버튼 제거
+            labelJson = []
+            dup = []
+            for i in range(len(box_label)):
+                if box_label[i] not in dup and box_label[i] != 'etc':
+                    labelJson.append((box_label[i], str(scores[i])[9:11]))
+                    dup.append(box_label[i])
 
-        # Result
-        result = ''
-        for l, s in labelJson:
-            result += l + ' ' + s + '%\n'
+            # Result
+            result = ''
+            for l, s in labelJson:
+                result += l + ' ' + s + '%\n'
+            if len(labelJson) == 0:
+                result = '이미지에서 식물을 찾을 수 없습니다. '
 
-        # API JSON
-        sendJson = {}
-        sendJson['version'] = '2.0'
+            # API JSON
+            sendJson = {}
+            sendJson['version'] = '2.0'
 
-        imageIP = os.environ.get('IMAGE_IP')
-        simpleImage = {}
-        simpleImage['imageUrl'] = imageIP
-        simpleImage['altText'] = '이미지 로딩 실패'
-        simpleImageJson = {'simpleImage': simpleImage}
+            imageIP = os.environ.get('IMAGE_IP')
+            simpleImage = {}
+            simpleImage['imageUrl'] = imageIP
+            simpleImage['altText'] = '이미지 로딩 실패'
+            simpleImageJson = {'simpleImage': simpleImage}
 
-        simpleTextJson = {}
-        simpleTextJson['simpleText'] = {'text': result[:-1]}
+            simpleTextJson = {}
+            simpleTextJson['simpleText'] = {'text': result[:-1]}
 
-        sendJson['template'] = {'outputs': [simpleImageJson, simpleTextJson]}
+            sendJson['template'] = {'outputs': [simpleImageJson, simpleTextJson]}
 
-        return JsonResponse(sendJson)
-'''
+            return JsonResponse(sendJson)
     except:
         if request.method == 'POST':
             # API JSON
@@ -127,10 +128,9 @@ def dino_api(request):
 
             simpleImageJson = {}
             # simpleImageJson['imageUrl'] = resultImgURL
-            simpleImageJson['simpleText'] = {'text': '식물 이미지를 올려주세요!'}
+            simpleImageJson['simpleText'] = {'text': '식물 이미지를 올려주세요! 이미지 인식이 되지 않을 경우 \'저용량\' 또는 \'일반 화질\'을 선택하여 올려주세요!'}
 
             sendJson['template'] = {'outputs': [simpleImageJson]}
             # sendJson['contents'] = [{'text': '식물 이미지를 올려주세요!', 'type': 'text', 'forwardable': True}]
 
             return JsonResponse(sendJson)
-'''
