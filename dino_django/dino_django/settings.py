9f8998ea9a2a46d8f506ce627f0e0771783933c2
hanjoo0211 <hanjoo0211@gmail.com>
2023-01-01 13:03:17 +0900

    fix: change media server

--- dino_django/dino_django/settings.py
+++ dino_django/dino_django/settings.py
@@ -20,6 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 ROOT_DIR = os.path.dirname(BASE_DIR)
 
+MEDIA_ROOT = BASE_DIR
+
+MEDIA_URL = 'media'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
